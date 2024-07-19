c5e0045055a72dfcf117a56f7b26a19e2b595dae
LvffY <37327656+LvffY@users.noreply.github.com>
2024-07-19 07:38:36 +0000

    Add third party requests lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
@@ -8,6 +9,9 @@ st.write(
     """Choose the fruits you want in your custom Smoothie !
     """
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
